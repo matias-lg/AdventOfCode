c6715f90e4d80a2d7271cfdf2a2b974e14e69758
matias-lg <matilop965@gmail.com>
2021-12-09 16:21:06 -0300

    one liner solution

---
+++ day7/2_compact.py
@@ -0,0 +1,8 @@
+from sys import argv
+from functools import reduce
+from math import floor
+
+l = [int(x) for x in open(argv[1], "r").read().split(",")]
+
+print(sum([sum([j for j in range(
+    1, abs((floor(reduce(lambda a, b: a+b, l)))//(len(l))-i)+1)]) for i in l]))
