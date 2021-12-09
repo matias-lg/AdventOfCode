d5f0e8de61067a3b20f22bed3d042b41610ee18f
matias-lg <matilop965@gmail.com>
2021-12-09 15:38:37 -0300

    day 7 finished

--- day7/2.py
+++ day7/2.py
@@ -1,23 +1,20 @@
 from sys import argv
+from functools import reduce
+from math import floor
 
 filename = argv[1]
 
 f = open(filename, "r").read()
 l = [int(x) for x in f.split(",")]
 
+avg = (floor(reduce(lambda a, b: a+b, l)/len(l)))
+print("avg:", avg)
 
-def mediana(a):
-    n = len(a)
-    s = sorted(a)
-    if n % 2 != 0:
-        return (s[int(n//2)])
-    return ((s[(n-1)//2] + s[n//2])//2)
-
-
-m = mediana(l)
 res = 0
 for i in l:
-    res += abs(i-m)
-
-print(mediana(l))
+    tot = 0
+    for i in range(1, abs(avg-i)+1):
+        tot += i
+    res += tot
 print(res)
+# for the example input use ceil instead of floor
