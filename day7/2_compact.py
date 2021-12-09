fdc69d25d3a494677a3cf827e421de3920bec4ad
matias-lg <matilop965@gmail.com>
2021-12-09 16:24:21 -0300

    one liner solution

--- day7/2_compact.py
+++ day7/2_compact.py
@@ -1,8 +1,6 @@
 from sys import argv
 from functools import reduce
 from math import floor
-
-l = [int(x) for x in open(argv[1], "r").read().split(",")]
-
 print(sum([sum([j for j in range(
-    1, abs((floor(reduce(lambda a, b: a+b, [int(x) for x in open(argv[1], "r").read().split(",")])))//(len(l))-i)+1)]) for i in [int(x) for x in open(argv[1], "r").read().split(",")]]))
+    1, abs((floor(reduce(lambda a, b: a+b, [int(x) for x in open(argv[1], "r").read().split(",")])))//(len([int(x) for x in open(argv[1], "r").read().split(",")]))-i)+1)]) for i in [int(x) for x in open(argv[1], "r").read().split(",")]]))
+# use ceil instead of floor for example input
